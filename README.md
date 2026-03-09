27e239bac49c482ed9033bcb58988a54a3f10db4
sseatwo <ssea2.heal@gmail.com>
2026-03-09 18:43:29 +0100

    chores(README.md): modification de l'arborescence des fichiers

--- README.md
+++ README.md
@@ -7,8 +7,13 @@ L'objectif est de faire un robot fonctionnel et fiable pour la Coupe de France d
 
 ## arborescence des fichiers
 
-R-aspirateur2000
-    - BaseRoulante: le code qui permet de déplacer le robot en prennant compte des colisions 
-    - doc: comme suggère le nom
-    - modele_3D_2D: comme suggère le nom
+- src : contient le code et les plan des pieces 3D pour concevoir un robot autonome pour la coupe
+- doc : la doc qui explique comment monté le robot
+- install : doit contenir les scripts pour setup le code du robot$
 
+
+
+# comment contribuer 
+
+1/ crée une branche suivant la "convention de nommage" Gitflow           
+2/ suivre les "conventional commit" pour push des changements
---
+++ src/code/BaseRoulante/raspi_main/raspi_config.h
@@ -0,0 +1,40 @@
+ #ifndef CONFIG_H
+#define CONFIG_H
+
+// parametre du robot
+#define COEFR2L 0.96 //0.96
+#define BIASR2L 1
+
+#define D_BETWEEN_WHEEL 222 //mm 
+#define CENTER2WHEEL 111 //mm
+#define DIAM_WHEEL  70.0 //mm
+#define nbPPR 341.2 // le nombre de pulsation des encodeurs par tour apres le réducteur 34:1
+
+#define NBMOTOR 2
+
+// ---------------PIN----------------
+
+// Pin motors Left
+#define L_EncoderA 6
+#define L_EncoderB 7
+#define L_MotorB 8
+#define L_MotorA 9
+
+// Pin motors Right
+#define R_EncoderA 2
+#define R_EncoderB 3
+#define R_MotorB 4
+#define R_MotorA 5
+
+
+// Collision and Distance
+// Left Sensor
+#define L_Collision A0
+#define L_Distance A1
+
+// Right Sensor
+#define R_Collision A2
+#define R_Distance A3
+
+
+#endif 
